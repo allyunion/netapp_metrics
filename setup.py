e076d169c360949b66780656fa7643168e733c0a
Jason Y. Lee <jylee@cs.ucr.edu>
2015-05-20 14:44:04 -0700

    Updating setup.py with some additional changes

--- setup.py
+++ setup.py
@@ -2,12 +2,28 @@
 
 from distutils.core import setup
 
-setup(name='netapp_ontap_api',
+setup(
+        name='netapp_ontap_api',
         version='0.1',
-        description='API wrapper for NetApp OnTAP API that understands '
-            'between cluster mode and non-cluster mode'
+        description='NetApp OnTAP API wrapper',
+        long_description='API wrapper for NetApp OnTAP API that understands '
+            'between cluster mode (C-mode) and non-cluster (7-mode) mode'
+        url='https://github.com/allyunion/netapp_ontap_api',
         author='Jason Y. Lee',
         author_email='jylee@cs.ucr.edu',
-        url='https://github.com/allyunion/netapp_ontap_api',
-        packages=['netapp_ontap_api'],
+        license='GPLv2',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries',
+            'License :: OSI Approved :: GNU General Public License v2',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+        keywords='netapp ontap api wrapper development',
+        py_modules= ['netapp_ontap_api', 'netapp_ontap_api.NetAppMetrics'],
     )
